5738743932033e3b6b18c2184a88194e10f31ffb
songrit <songrit@gmail.com>
2011-04-04 14:35:08 +0700

    fix: fusionchart helper, gma_runseq (rstep), signup form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,5 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  require "fusion_charts_helper"
-
   def province_prefix(province_id)
     return province_id==1 ? "" : "จังหวัด"
   end
