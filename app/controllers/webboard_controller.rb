7790f0ea4d2418407e68e3052d66381813a60a4a
songrit <songrit@gmail.com>
2011-04-23 07:13:17 +0700

    start work on webboard

--- app/controllers/webboard_controller.rb
+++ app/controllers/webboard_controller.rb
@@ -1,5 +1,29 @@
 class WebboardController < ApplicationController
   def index
-    redirect_to :controller => "main", :action => "coming_soon"
+    # redirect_to :controller => "main", :action => "coming_soon"
+    @notices= Notice.paginate :per_page => 20, :page => params[:page] 
+  end
+  def create_notice
+    notice= Notice.new $xvars[:enter][:notice]
+    img= $xvars[:enter][:notice_pic_doc_id]
+    if img
+      scale_image(img)
+      @pic_postimg= postimg(File.join(IMAGE_LOCATION,"f#{img}"))
+      notice.pic= @pic_postimg
+    end
+    notice.save
+    $xvars[:p][:return]= '/webboard'
+  end
+  
+  private
+  def scale_image(img)
+    max_cols= 680
+    f= "#{IMAGE_LOCATION}/f#{img}"
+    img = Magick::Image.read(f).first
+    if img.columns > max_cols
+      img = img.change_geometry(max_cols){|cols,rows,i| i.resize!(cols,rows)}
+      img.write(f)
+    end
+    img.destroy!
   end
 end
--- spec/controllers/webboard_controller_spec.rb
+++ spec/controllers/webboard_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe WebboardController do
-  it "should manage web board"
+  describe "controller actions" do
+    integrate_views
+    before(:each) do
+      @notice= mock_model(Notice, :subject=>'aa', :body => "bb", :created_at=>Time.now)
+    end
+    it "should show index with paginate" do
+      Notice.should_receive(:paginate).and_return([@notice])
+      get :index
+      response.should have_tag('a', @notice.subject)
+    end
+    it "should show each notice"
+  end
+  describe "gma methods" do
+    before(:each) do
+      $xvars= {:p=>{}}
+      $xvars[:enter]= {:subject=>'aa', :body => "bb" }
+    end
+    it "should manage web board" do
+      post :create_notice
+    end
+  end
 end
